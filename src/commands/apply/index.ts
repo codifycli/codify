af041d58941dc2da8c269099e0aa765cbc86e23d
kevinwang <kevinwang5658@gmail.com>
2024-05-07 23:58:30 -0400

    Added an error catcher to plan and apply commands

--- src/commands/apply/index.ts
+++ src/commands/apply/index.ts
@@ -18,50 +18,41 @@ export default class Apply extends Command {
   ]
 
   static flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' }),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: 'n', description: 'name to print' }),
     // flag with a value (-p, --path=VALUE)
     path: Flags.string({ char: 'p', description: 'path to project' }),
   }
 
   public async run(): Promise<void> {
-    const { args, flags } = await this.parse(Apply)
+    const { flags } = await this.parse(Apply)
     const reporter = new DefaultReporter()
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/kevinwang/Projects/codify/codify-core/src/commands/apply.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    try {
+      if (flags.path) {
+        this.log(`Applying Codify from: ${flags.path}`);
+      }
+
+      const resolvedPath = path.resolve(flags.path ?? '.');
+
+      const planResult = await PlanOrchestrator.run(resolvedPath, false);
+      reporter.displayPlan(planResult.plan);
+
+      // Short circuit and exit if every change is NOOP
+      if (planResult.plan.every((p) => p.operation === ResourceOperation.NOOP)) {
+        console.log('No changes necessary. Exiting');
+        await planResult.pluginCollection.destroy();
+        return this.exit(0);
+      }
+
+      const confirm = await reporter.promptApplyConfirmation()
+      if (!confirm) {
+        return this.exit(0);
+      }
+
+      await ApplyOrchestrator.run(planResult);
+    } catch (error: unknown) {
+      console.error(error);
     }
 
-    if (flags.path) {
-      this.log(`Applying Codify from: ${flags.path}`);
-    }
-
-    const resolvedPath = path.resolve(flags.path ?? '.');
-
-    const planResult = await PlanOrchestrator.run(resolvedPath, false);
-
-    // Short circuit and exit if every change is NOOP
-    if (planResult.plan.every((p) => p.operation === ResourceOperation.NOOP)) {
-      console.log('No changes necessary. Exiting');
-      await planResult.pluginCollection.destroy();
-      return;
-    }
-
-    reporter.displayPlan(planResult.plan);
-
-    const confirm = await reporter.promptApplyConfirmation()
-
-    if (!confirm) {
-      return this.exit(0);
-    }
-
-    await ApplyOrchestrator.run(planResult);
-
-    // this.exit(0);
-
+    this.exit(0);
   }
 }
