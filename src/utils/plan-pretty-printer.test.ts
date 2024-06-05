e50144f1d9c8cf4bffe3d9dc523043f6d16cebb2
kevinwang <kevinwang5658@gmail.com>
2024-06-04 23:21:31 -0400

    [CODE-34] Added pretty print plan function + tests

---
+++ src/utils/plan-pretty-printer.test.ts
@@ -0,0 +1,73 @@
+import { describe, it } from 'vitest';
+import { prettyFormatPlan } from './plan-pretty-printer.js';
+import { ParameterOperation, PlanResponseData, ResourceOperation } from 'codify-schemas';
+
+describe('Plan pretty printer', () => {
+  it('Can print create plans', () => {
+    const plan: PlanResponseData = {
+      planId: 'id',
+      operation: ResourceOperation.CREATE,
+      parameters: [
+        { name: 'propC', previousValue: null, newValue: 'yui', operation: ParameterOperation.ADD },
+        { name: 'propD', previousValue: null, newValue: 'qwe', operation: ParameterOperation.ADD },
+        {
+          name: 'propE',
+          previousValue: null,
+          newValue: ['10.0.0', '11.0.0', '9.0.0'],
+          operation: ParameterOperation.ADD
+        },
+        { name: 'propF', previousValue: null, newValue: ['abc', 'def'], operation: ParameterOperation.ADD },
+      ]
+    }
+
+    console.log(prettyFormatPlan(plan))
+  })
+
+  it('Can print destroy plans', () => {
+    const plan: PlanResponseData = {
+      planId: 'id',
+      operation: ResourceOperation.DESTROY,
+      parameters: [
+        { name: 'propC', previousValue: 'yui', newValue: null, operation: ParameterOperation.REMOVE },
+        { name: 'propD', previousValue: 'qwe', newValue: null, operation: ParameterOperation.REMOVE },
+        {
+          name: 'propE',
+          previousValue: ['10.0.0', '11.0.0', '9.0.0'],
+          newValue: null,
+          operation: ParameterOperation.REMOVE
+        },
+        { name: 'propF', previousValue: ['abc', 'def'], newValue: null, operation: ParameterOperation.REMOVE },
+      ]
+    }
+
+    console.log(prettyFormatPlan(plan))
+  })
+
+  it('Can print modify and re-create plans', () => {
+    const plan: PlanResponseData = {
+      planId: 'id',
+      operation: ResourceOperation.RECREATE,
+      parameters: [
+        { name: 'propA', previousValue: 'abc', newValue: 'def', operation: ParameterOperation.MODIFY },
+        {
+          name: 'propALong',
+          previousValue: 'abc\ndef',
+          newValue: 'def\nteoewriu',
+          operation: ParameterOperation.MODIFY
+        },
+        { name: 'propB', previousValue: 'xzy', newValue: 'xzy', operation: ParameterOperation.NOOP },
+        { name: 'propC', previousValue: null, newValue: 'yui', operation: ParameterOperation.ADD },
+        { name: 'propD', previousValue: 'qwe', newValue: null, operation: ParameterOperation.REMOVE },
+        {
+          name: 'propE',
+          previousValue: ['10.0.0', '9.0.0'],
+          newValue: ['10.0.0', '11.0.0'],
+          operation: ParameterOperation.MODIFY
+        },
+        { name: 'propF', previousValue: null, newValue: ['abc', 'def'], operation: ParameterOperation.ADD },
+      ]
+    }
+
+    console.log(prettyFormatPlan(plan))
+  })
+});
