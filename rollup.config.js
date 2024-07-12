77dd87721d8e1dafdddefbcc584db01dfa5c8da7
kevinwang <kevinwang5658@gmail.com>
2024-07-11 22:20:50 -0400

    Improved CLI performance by using rollup to bundle, tree-shake and minify files

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import wasm from '@rollup/plugin-wasm';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir:'dist',
+    format: 'es'
+  },
+  plugins: [
+    copy({
+      targets: [{ dest: './dist/', src: './node_modules/yoga-wasm-web/dist/yoga.wasm' }]
+    }),
+    json(),
+    wasm(),
+    nodeResolve({ exportConditions: ['node'] }),
+    typescript(),
+    commonjs(),
+    terser()
+  ]
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export { run } from '@oclif/core'
+import { Command, execute } from '@oclif/core';
+
+import Apply from './commands/apply/index.js'
+import Plan from './commands/plan/index.js'
+import Uninstall from './commands/uninstall.js'
+
+export const COMMANDS: Record<string, Command.Class> = {
+  apply: Apply,
+  plan: Plan,
+  uninstall: Uninstall,
+}
+
+export async function run() {
+  await execute({ dir: import.meta.url })
+}
