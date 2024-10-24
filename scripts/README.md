ad2ff3c6b055f4c4c5a10add20937007d288db8a
kevinwang <kevinwang5658@gmail.com>
2024-10-23 22:47:00 -0400

    feat: add notarize script

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+## Notarize script
+
+Set up notarize profiles first (see notion).
+Run notarize script from scripts folder.
+Make sure that issues is null in the log.
---
+++ scripts/notarize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+for filename in ../dist/macos/*.pkg; do
+  echo "Uploading and notarizing $filename with Apple... (5-10 minutes)"
+  RESULT=$(
+    xcrun notarytool submit $filename \
+    --keychain-profile "notarytool-password" \
+    --wait \
+    -f json
+  )
+
+  echo "Nortary tool submit completed with result:"
+  echo $RESULT
+
+  SUBMISSION_ID=$(jq -r '.id' <<< $RESULT)
+  xcrun notarytool log $SUBMISSION_ID --keychain-profile "notarytool-password"
+
+  echo "Stapling notary to $filename"
+  xcrun stapler staple $filename
+  echo "Done stapling"
+done
+
