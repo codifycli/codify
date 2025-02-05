19a8af29a11cc92883945a2e3e7549a68bc571fe
kevinwang <kevinwang5658@gmail.com>
2025-02-04 22:02:19 -0500

    feat: WIP added ability to delete a resource from an existing file

--- src/utils/file-modification-calculator.test.ts
+++ src/utils/file-modification-calculator.test.ts
@@ -1,8 +1,12 @@
-import { describe, it, vi, afterEach } from 'vitest';
-import { FileModificationCalculator, ModificationType } from './file-modification-calculator';
-import { ResourceConfig } from '../entities/resource-config';
-import { ResourceInfo } from '../entities/resource-info';
-import { FileType, InMemoryFile } from '../parser/entities';
+import * as fs from 'node:fs';
+import * as os from 'node:os';
+import * as path from 'node:path';
+
+import { describe, it, vi, afterEach, expect } from 'vitest';
+import { FileModificationCalculator, ModificationType } from './file-modification-calculator.js';
+import { ResourceConfig } from '../entities/resource-config.js';
+import { ResourceInfo } from '../entities/resource-info.js';
+import { CodifyParser } from '../parser/index.js';
 
 vi.mock('node:fs', async () => {
   const { fs } = await import('memfs');
@@ -14,26 +18,28 @@ vi.mock('node:fs/promises', async () => {
   return fs.promises;
 })
 
+const defaultPath = '/codify.json'
+
 
 describe('File modification calculator tests', () => {
 
   it('Can generate a diff and a new file', async () => {
-    const existingResource = new ResourceConfig({
-      type: 'resource1'
-    });
-    existingResource.attachResourceInfo(generateResourceInfo('resource1'))
-
-    const existingFileContents =
+    const existingFile =
 `[
   {
-    "type": "project",
-    "plugins": {
-      "default": "latest",
-    }
-  },
-  { "type": "resource1" }
+		"type": "project",
+		"plugins": {
+			"default": "latest"
+		}
+	},
+	{ "type": "resource1", "param2": ["a", "b", "c"]}
 ]`
-    const existingFile = <InMemoryFile> { filePath: '/path/to/file.json', fileType: FileType.JSON, contents: existingFileContents };
+    generateTestFile(existingFile);
+
+    const project = await CodifyParser.parse(defaultPath)
+    project.resourceConfigs.forEach((r) => {
+      r.attachResourceInfo(generateResourceInfo(r.type))
+    });
 
     const modifiedResource = new ResourceConfig({
       type: 'resource1',
@@ -41,12 +47,104 @@ describe('File modification calculator tests', () => {
     })
     modifiedResource.attachResourceInfo(generateResourceInfo('resource1'))
 
-    const calculator = new FileModificationCalculator([existingResource], existingFile)
+    const calculator = new FileModificationCalculator(project.resourceConfigs, project.sourceMaps.getSourceMap(defaultPath).file, project.sourceMaps);
     const result = await calculator.calculate([{
       modification: ModificationType.INSERT_OR_UPDATE,
       resource: modifiedResource,
     }])
 
+    console.log(result)
+    console.log(result.diff)
+  })
+
+  it('Can delete a resource from an existing config (with proper commas)', async () => {
+    const existingFile =
+`[
+  {
+    "type": "project",
+    "plugins": {
+      "default": "latest"
+    }
+  },
+  { 
+    "type": "resource1",
+    "param2": ["a", "b", "c"]
+  }
+]`
+    generateTestFile(existingFile);
+
+    const project = await CodifyParser.parse(defaultPath)
+    project.resourceConfigs.forEach((r) => {
+      r.attachResourceInfo(generateResourceInfo(r.type))
+    });
+
+    const modifiedResource = new ResourceConfig({
+      type: 'resource1',
+      parameter1: 'abc'
+    })
+    modifiedResource.attachResourceInfo(generateResourceInfo('resource1'))
+
+    const calculator = new FileModificationCalculator(project.resourceConfigs, project.sourceMaps.getSourceMap(defaultPath).file, project.sourceMaps);
+    const result = await calculator.calculate([{
+      modification: ModificationType.DELETE,
+      resource: modifiedResource,
+    }])
+
+    expect(result.newFile).to.eq('[\n' +
+      '  {\n' +
+      '    "type": "project",\n' +
+      '    "plugins": {\n' +
+      '      "default": "latest"\n' +
+      '    }\n' +
+      '  }\n' +
+      '  \n' +
+      ']')
+    console.log(result)
+    console.log(result.diff)
+  })
+
+  it('Can delete a resource from an existing config 2 (with proper commas)', async () => {
+    const existingFile =
+      `[
+  { 
+    "type": "resource1",
+    "param2": ["a", "b", "c"]
+  },
+  {
+    "type": "project",
+    "plugins": {
+      "default": "latest"
+    }
+  }
+]`
+    generateTestFile(existingFile);
+
+    const project = await CodifyParser.parse(defaultPath)
+    project.resourceConfigs.forEach((r) => {
+      r.attachResourceInfo(generateResourceInfo(r.type))
+    });
+
+    const modifiedResource = new ResourceConfig({
+      type: 'resource1',
+      parameter1: 'abc'
+    })
+    modifiedResource.attachResourceInfo(generateResourceInfo('resource1'))
+
+    const calculator = new FileModificationCalculator(project.resourceConfigs, project.sourceMaps.getSourceMap(defaultPath).file, project.sourceMaps);
+    const result = await calculator.calculate([{
+      modification: ModificationType.DELETE,
+      resource: modifiedResource,
+    }])
+
+    // expect(result.newFile).to.eq('[\n' +
+    //   '  {\n' +
+    //   '    "type": "project",\n' +
+    //   '    "plugins": {\n' +
+    //   '      "default": "latest"\n' +
+    //   '    }\n' +
+    //   '  }\n' +
+    //   '  \n' +
+    //   ']')
     console.log(result)
     console.log(result.diff)
   })
@@ -62,4 +160,11 @@ function generateResourceInfo(type: string, requiredParameters?: string[]): Reso
     type,
     import: { requiredParameters }
   })
+}
+
+/**
+ * To generate the source maps and parsed resources it's easier to write it to the file-system and parse it for real
+ */
+function generateTestFile(contents: string, filePath = defaultPath): void {
+  fs.writeFileSync(filePath, contents, { encoding: 'utf8' });
 }
--- src/utils/file-modification-calculator.ts
+++ src/utils/file-modification-calculator.ts
@@ -2,6 +2,8 @@ import chalk from 'chalk';
 import { ResourceConfig } from '../entities/resource-config.js';
 import * as Diff from 'diff'
 import { FileType, InMemoryFile } from '../parser/entities.js';
+import { SourceLocation, SourceMapCache } from '../parser/source-maps.js';
+import detectIndent from 'detect-indent';
 
 export enum ModificationType {
   INSERT_OR_UPDATE,
@@ -21,10 +23,12 @@ export interface FileModificationResult {
 export class FileModificationCalculator {
   private existingFile?: InMemoryFile;
   private existingResources: ResourceConfig[];
+  private sourceMaps: SourceMapCache;
 
-  constructor(existingResources: ResourceConfig[], existingFile: InMemoryFile) {
+  constructor(existingResources: ResourceConfig[], existingFile: InMemoryFile, sourceMaps: SourceMapCache) {
     this.existingFile = existingFile;
     this.existingResources = existingResources;
+    this.sourceMaps = sourceMaps;
   }
 
   async calculate(modifications: ModifiedResource[]): Promise<FileModificationResult> {
@@ -44,41 +48,49 @@ export class FileModificationCalculator {
     }
 
     this.validate(modifications);
+    const { sourceMap, file } = this.sourceMaps.getSourceMap('/codify.json')!;
+    const fileIndents = detectIndent(file.contents);
+    const indentString = fileIndents.indent;
 
-    for (const modified of modifications) {
+    let newFile = file.contents.trimEnd();
+
+    console.log(JSON.stringify(sourceMap, null, 2))
+
+    // Reverse the traversal order so we edit from the back. This way the line numbers won't be messed up with new edits.
+    for (const modified of modifications.reverse()) {
       const duplicateIndex = this.existingResources.findIndex((existing) => existing.isSameOnSystem(modified.resource))
 
       if (duplicateIndex === -1) {
         if (modified.modification === ModificationType.INSERT_OR_UPDATE) {
-          resultResources.push(modified.resource);
+          const config = JSON.stringify(modified.resource.raw, null, indentString)
+          newFile = this.insertConfig(newFile, config, indentString);
         }
 
         continue;
       }
 
+      const duplicate = this.existingResources[duplicateIndex];
+      const duplicateSourceKey = duplicate.sourceMapKey?.split('#').at(1)!;
+
       if (modified.modification === ModificationType.DELETE) {
-        resultResources.splice(duplicateIndex, 1);
+        const { value, valueEnd } = sourceMap.lookup(duplicateSourceKey)!
+
+        newFile = this.remove(newFile, value, valueEnd);
         continue;
+
       }
 
-      const duplicate = resultResources[duplicateIndex];
-      for (const [key, newValue] of Object.entries(modified.resource.parameters)) {
-        duplicate.setParameter(key, newValue);
-      }
+      resultResources.splice(duplicateIndex, 1, modified.resource);
     }
 
-    const newFile = JSON.stringify(
-      resultResources.map((r) => r.raw),
-      null, 2
-    );
-
     return {
-      newFile,
+      newFile: newFile,
       diff: this.diff(this.existingFile.contents, newFile),
     }
   }
 
   validate(modifiedResources: ModifiedResource[]): void {
+    // The result of the validation rules only apply if we want to insert into a file. If it's a new file nothing is really needed
     if (!this.existingFile) {
       return;
     }
@@ -102,6 +114,10 @@ export class FileModificationCalculator {
 
       throw new Error(`All resources must have resource info attached to generate diff. Found bad resources: ${badResources}`);
     }
+
+    if (!this.sourceMaps) {
+      throw new Error('Source maps must be provided to generate new code');
+    }
   }
 
   diff(a: string, b: string): string {
@@ -115,5 +131,55 @@ export class FileModificationCalculator {
     });
 
     return result;
+  }
+
+  // Insert always works at the end
+  private insertConfig(
+    file: string,
+    config: string,
+    indentString: string,
+  ) {
+    const configWithIndents = config.split(/\n/).map((l) => `${indentString}l`).join('\n');
+    const result = file.substring(0, configWithIndents.length - 1) + ',' + configWithIndents + file.at(-1);
+
+    // Need to fix the position of the comma
+
+    return result;
+  }
+
+  private remove(
+    file: string,
+    value: SourceLocation,
+    valueEnd: SourceLocation,
+  ): string {
+    let result = file.substring(0, value.position) + file.substring(valueEnd.position)
+
+    let commaIndex = - 1;
+    for (let counter = value.position; counter > 0; counter--) {
+      if (result[counter] === ',') {
+        commaIndex = counter;
+        break;
+      }
+    }
+
+    // Not able to find comma behind (this was the first element). We want to delete the comma behind then.
+    if (commaIndex === -1) {
+      for (let counter = value.position; counter < file.length - 1; counter++) {
+        if (result[counter] === ',') {
+          commaIndex = counter;
+          break;
+        }
+      }
+    }
+
+    if (commaIndex !== -1) {
+      result = this.splice(result, commaIndex, 1)
+    }
+
+    return result;
+  }
+
+  private splice(s: string, start: number, deleteCount = 0, insert = '') {
+    return s.substring(0, start) + insert + s.substring(start + deleteCount);
   }
 }
