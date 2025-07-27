d37f10d5d413905fdf4e6319dfaa3036b62d724b
kevinwang <kevinwang5658@gmail.com>
2025-07-27 19:37:42 -0400

    feat: switched to hono server and added cors support

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  loginServerPort: 51_039,
+  corsAllowedOrigins: [
+    'https://codify-dashboard.com',
+    'https://https://codify-dashboard.kevinwang5658.workers.dev',
+    'http://localhost:3000'
+  ]
+}
