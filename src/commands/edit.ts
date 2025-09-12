f2c1bf8afd10a65dc38c2607a41c25c113285ac1
kevinwang <kevinwang5658@gmail.com>
2025-09-12 17:33:59 -0400

    Fixed login bugs. - Resolve promise when logged in - Fixed login manager bug Added useful log message for connect

---
+++ src/commands/edit.ts
@@ -0,0 +1,23 @@
+import { BaseCommand } from '../common/base-command.js';
+import { ConnectOrchestrator } from '../orchestrators/connect.js';
+
+export default class Edit extends BaseCommand {
+  static description =
+    `Edit a codify.jsonc/codify.json/codify.yaml file.
+    
+For more information, visit: https://docs.codifycli.com/commands/validate
+`
+
+  static flags = {}
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+    '<%= config.bin %> <%= command.id %> --path=../../import.codify.jsonc',
+  ]
+
+  public async run(): Promise<void> {
+    const { flags } = await this.parse(Edit)
+
+    await ConnectOrchestrator.run();
+  }
+}
