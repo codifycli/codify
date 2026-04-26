9eab689095d4fe1cdc462da87b1859e20c75aece
kevinwang <kevinwang5658@gmail.com>
2026-04-26 14:49:53 -0400

    feat: Added terminal resize handler

--- src/connect/http-routes/router.ts
+++ src/connect/http-routes/router.ts
@@ -6,6 +6,7 @@ import defaultHandler from './handlers/index.js';
 import { initHandler } from './handlers/init-handler.js';
 import { planHandler } from './handlers/plan-handler.js';
 import { refreshHandler } from './handlers/refresh-handler.js';
+import { resizeHandler } from './handlers/resize-handler.js';
 import { terminalHandler } from './handlers/terminal-handler.js';
 import { testHandler } from './handlers/test-handler.js';
 
@@ -16,6 +17,7 @@ router.use('/apply', applyHandler());
 router.use('/plan', planHandler())
 router.use('/import', importHandler());
 router.use('/refresh', refreshHandler());
+router.use('/resize', resizeHandler());
 router.use('/terminal', terminalHandler());
 router.use('/init', initHandler());
 router.use('/test', testHandler());
