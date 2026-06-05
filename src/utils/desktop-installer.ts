793cb91eedfbfd1af7b6756c36819cba2da93175
kevinwang <kevinwang5658@gmail.com>
2026-06-04 21:01:57 -0400

    chore: update download links for desktop app

--- src/utils/desktop-installer.ts
+++ src/utils/desktop-installer.ts
@@ -8,22 +8,22 @@ import { OsUtils } from './os-utils.js';
 import { spawn } from './spawn.js';
 
 const DESKTOP_APP_PATHS = {
-  darwin: '/Applications/Codify.app',
+  darwin: '/Applications/CodifyApp.app',
   linux: '/usr/bin/codify-desktop',
 };
 
 const DOWNLOAD_URLS: Record<string, Record<string, string>> = {
   darwin: {
-    arm64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_aarch64.dmg',
-    x64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_x64.dmg',
+    arm64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_aarch64.dmg',
+    x64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_x64.dmg',
   },
   linux_deb: {
-    arm64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_arm64.deb',
-    x64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_amd64.deb',
+    arm64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_arm64.deb',
+    x64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_amd64.deb',
   },
   linux_rpm: {
-    aarch64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_aarch64.rpm',
-    x64: 'https://releases-desktop.codifycli.com/channels/stable/Codify_x86_64.rpm',
+    aarch64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_aarch64.rpm',
+    x64: 'https://releases-desktop.codifycli.com/channels/stable/CodifyApp_x86_64.rpm',
   },
 };
 
@@ -79,7 +79,7 @@ export async function installDesktopApp(reporter: Reporter, url: string, platfor
     try {
       console.log('Installing Codify desktop app...');
       await spawn(`hdiutil attach ${tmpFile} -mountpoint ${mountPoint} -nobrowse -quiet`);
-      await spawn(`cp -R ${mountPoint}/Codify.app /Applications/Codify.app`);
+      await spawn(`cp -R ${mountPoint}/CodifyApp.app /Applications/CodifyApp.app`);
     } finally {
       await spawn(`hdiutil detach ${mountPoint} -quiet`).catch(() => {});
       await fs.unlink(tmpFile).catch(() => {});
